c321379e888df3dc22eabc7816ad9fd03b438da9
deathrat <deathrat43@gmail.com>
2013-01-23 14:00:25 -0500

    Added missing classes

---
+++ src/minecraft/deathrat/mods/btbees/world/WorldGen.java
@@ -0,0 +1,54 @@
+package deathrat.mods.btbees.world;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenClay;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.IWorldGenerator;
+import deathrat.mods.btbees.BetterThanBees;
+
+public class WorldGen implements IWorldGenerator
+{
+	public WorldGenClay saltGen = new WorldGenClay(BetterThanBees.saltID);
+
+	@Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
+    {
+//		BiomeGenBase biome = world.getBiomeGenForCoords(chunkX, chunkZ);
+//		String biomeName = biome.biomeName;
+//		int x = chunkX + random.nextInt(16) + 8;
+//		int y = random.nextInt(128);
+//		int z = chunkZ + random.nextInt(16) + 8;
+
+
+		if(world.provider.dimensionId == 0)
+		{
+			generateSalt(world, random, chunkX, chunkZ);
+
+		}
+    }
+
+	private void generateSalt(World world, Random random, int chunkX, int chunkZ)
+    {
+		for(int i = 0; i < 30; i++)
+		{
+			int randomX = chunkX + random.nextInt(16) + 8;
+			int randomZ = chunkZ + random.nextInt(16) + 8;
+			int randomY = world.getTopSolidOrLiquidBlock(randomX, randomZ);
+
+			int topBlock = world.getBlockId(randomX, randomY, randomZ);
+
+
+			if(topBlock == Block.sand.blockID)
+				saltGen.generate(world, random, randomX, world.getTopSolidOrLiquidBlock(randomX, randomZ), randomZ);
+		}
+    }
+
+
+
+}
---
+++ src/minecraft/deathrat/mods/btbees/world/WorldGenSalt.java
@@ -0,0 +1,61 @@
+package deathrat.mods.btbees.world;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import deathrat.mods.btbees.BetterThanBees;
+
+public class WorldGenSalt extends WorldGenerator
+{
+    /** The block ID for clay. */
+    private int clayBlockId;
+
+    /** The number of blocks to generate. */
+    private int numberOfBlocks;
+
+    public WorldGenSalt(int par1)
+    {
+        this.clayBlockId = BetterThanBees.salt.blockID;
+        this.numberOfBlocks = par1;
+    }
+
+    public boolean generate(World world, Random random, int x, int y, int z)
+    {
+        if (world.getBlockId(x, y - 1, z) != Block.sand.blockID )
+        {
+            return false;
+        }
+        else
+        {
+            int randomX = random.nextInt(this.numberOfBlocks - 2) + 2;
+            byte var7 = 1;
+
+            for (int tempX = x - randomX; tempX <= x + randomX; ++tempX)
+            {
+                for (int tempZ = z - randomX; tempZ <= z + randomX; ++tempZ)
+                {
+                    int newTempX = tempX - x;
+                    int newTempZ = tempZ - z;
+
+                    if (newTempX * newTempX + newTempZ * newTempZ <= randomX * randomX)
+                    {
+                        for (int var12 = y - var7; var12 <= y + var7; ++var12)
+                        {
+                            int var13 = world.getBlockId(tempX, var12, tempZ);
+
+                            if (var13 == Block.dirt.blockID || var13 == Block.blockClay.blockID)
+                            {
+                                world.setBlock(tempX, var12, tempZ, this.clayBlockId);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
